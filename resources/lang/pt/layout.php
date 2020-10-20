f729eadbf5081c5f058a21e587d9061698078869
damianicely <damiancely@gmail.com>
2020-10-20 16:19:35 +0100

    first language

---
+++ resources/lang/pt/layout.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Layout Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various structural elements of
+    | the website. In particular anything that is shared across multiple pages.
+    |
+    */
+
+    // Footer
+    'foot-love'=>'Criado com',
+    'foot-by'=>'por',
+    ];
+?>
