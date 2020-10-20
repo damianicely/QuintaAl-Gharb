24e0512a075e4b729835448d68bb8bbe7bc6d2af
damianicely <damiancely@gmail.com>
2020-10-20 12:55:55 +0100

    first working site version

---
+++ resources/views/home.blade.php
@@ -0,0 +1,181 @@
+@extends('layout')
+
+@section('title')
+Quinta Al-Gharb
+@endsection
+
+@section('body')
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+        <section class="py-0" id="cafe-header">
+            <div class="bg-holder " style="background-image:url(/img/quinta/surrounding3.jpg);"></div>
+            <!--/.bg-holder-->
+            <div class="container overflow-hidden">
+              <div data-zanim-timeline='{"delay":0.6}' data-zanim-trigger="scroll">
+                <div class="row min-vh-100 align-items-center py-8 justify-content-center text-center">
+                  <div class="col-lg-8">
+                    <div class="overflow-hidden">
+                      <h1 class="text-white fs-3 fs-sm-4 fs-lg-6 px-2 px-lg-0 parallax display-4" data-rellax-speed="5">
+                        <span class="overflow-hidden d-block"><span class="d-inline-block customfont" data-zanim-xs='{"delay":0.1}'>Quinta Al-Gharb</span></span>
+                      </h1>
+                      <h1 class="text-white fs-3 fs-sm-4 fs-lg-2 px-2 px-lg-0 parallax display-4" data-rellax-speed="5">
+                        <span class="overflow-hidden d-block"><span class="d-inline-block" data-zanim-xs='{"delay":0.2}'>NATURE - BEACH -RESORT</span></span>
+                      </h1>
+                    </div>
+                    <div class="overflow-hidden parallax" data-rellax-speed="3">
+                      <h5 class="fs-0 fs-sm-1 mt-3 mt-md-5 text-white" data-zanim-xs='{"delay":0.4}'>ALGARVE | PORTUGAL</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a class="indicator indicator-down" data-zanim-timeline='{"delay":2}' data-zanim-trigger="scroll" href="#target-down" data-fancyscroll="data-fancyscroll"><span class="indicator-arrow indicator-arrow-one" data-zanim-xs='{"from":{"opacity":0,"y":15},"to":{"opacity":1,"y":-5,"scale":1},"ease":"Back.easeOut","duration":0.4,"delay":0.25}'></span><span class="indicator-arrow indicator-arrow-two" data-zanim-xs='{"from":{"opacity":0,"y":15},"to":{"opacity":1,"y":-5,"scale":1},"ease":"Back.easeOut","duration":0.4,"delay":0.5}'></span></a>
+            </div>
+            <!-- end of .container-->
+          </section>
+          <!-- <section> close ============================-->
+          <!-- ============================================-->
+    
+          <div id="target-down"></div>
+    
+          <!-- ============================================-->
+          <!-- <section> begin ============================-->
+          <section class="text-center pb-0" id="cafe-about">
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-9 overflow-hidden">
+                  <p class="fs-2 text-sans-serif font-weight-normal text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deserunt necessitatibus perferendis nam. Veniam temporibus doloribus voluptatibus tempora. Cupiditate ullam molestias ea dignissimos sequi mollitia molestiae veritatis dolor! Animi, sed?</p>
+                </div>
+              </div>
+              <div class="row mt-6">
+                <div class="col-6 col-lg-3">
+                  <img class="rounded img-fluid" src="/img/quinta/bed1.jpg" alt="" data-zanim-xs='{"animation":"zoom-in","delay":0.1}' data-zanim-trigger="scroll" />
+                </div>
+                <div class="col-6 col-lg-3">
+                  <img class="rounded img-fluid" src="/img/quinta/kitchen2.jpg" alt="" data-zanim-xs='{"animation":"zoom-in","delay":0.2}' data-zanim-trigger="scroll" />
+                </div>
+                <div class="col-6 col-lg-3 mt-4 mt-lg-0">
+                  <img class="rounded img-fluid" src="/img/quinta/bed3.jpg" alt="" data-zanim-xs='{"animation":"zoom-in","delay":0.3}' data-zanim-trigger="scroll" />
+                </div>
+                <div class="col-6 col-lg-3 mt-4 mt-lg-0">
+                  <img class="rounded img-fluid" src="/img/quinta/kitchen4.jpg" alt="" data-zanim-xs='{"animation":"zoom-in","delay":0.4}' data-zanim-trigger="scroll" />
+                </div>
+              </div>
+              <div class="row justify-content-center mt-6">
+                <div class="col-lg-9 overflow-hidden">
+                  <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt commodi dolorum eveniet nulla. Placeat alias natus dolores eveniet soluta minus, incidunt obcaecati eligendi at, repellendus, itaque enim commodi mollitia illum.</p>
+                </div>
+              </div>
+              <div class="row mt-6 no-gutters">
+                <div class="col-6 col-sm-4 px-2">
+                  <img class="rounded img-fluid" src="/img/quinta/surrounding1.jpg" alt="" data-zanim-xs='{"animation":"zoom-in","delay":0.1}' data-zanim-trigger="scroll" />
+                </div>
+                <div class="col-6 col-sm-4 px-2">
+                  <img class="rounded img-fluid" src="/img/quinta/surrounding2.jpg" alt="" data-zanim-xs='{"animation":"zoom-in","delay":0.2}' data-zanim-trigger="scroll" />
+                </div>
+                <div class="col-sm-4 mt-3 mt-sm-0 px-2">
+                  <img class="rounded img-fluid" src="/img/quinta/surrounding3.jpg" alt="" data-zanim-xs='{"animation":"zoom-in","delay":0.3}' data-zanim-trigger="scroll" />
+                </div>
+              </div>
+              <div class="row justify-content-center mt-6">
+                <div class="col-lg-9 overflow-hidden">
+                  <p class="fs-2 text-sans-serif font-weight-normal text-dark" data-zanim-xs='{"delay":0.1}' data-zanim-trigger="scroll">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque illum minus blanditiis sequi est hic, aut ducimus tempora pariatur quis a perferendis officiis eum assumenda? Laboriosam quos ab voluptatum sint!</p>
+                </div>
+              </div>
+            </div>
+            <!-- end of .container-->
+          </section>
+          <!-- <section> close ============================-->
+          <!-- ============================================-->
+    
+    
+          <!-- ============================================-->
+          <!-- <section> begin ============================-->
+          <section class="py-0" id="cafe-banner">
+            <div class="container-fluid">
+              <div class="row align-items-center">
+                <div class="col-lg-6 py-5 py-lg-11 d-flex justify-content-center text-center text-lg-left">
+                  <h1 class="fs-4 fs-sm-5 font-weight-light display-4">Janta
+                    <br class="d-none d-lg-block" />connosco
+                    <br class="d-none d-lg-block" /><span class="font-weight-black text-underline">peixe</span> assado!</h1>
+                </div>
+                <div class="col-lg-6 ml-auto px-0">
+                  <div class="parallax" data-rellax-percentage="0.5" data-rellax-speed="3">
+                    <div class="py-11 shadow-lg" data-zanim-xs='{"delay":0.1,"animation":"zoom-out"}' data-zanim-lg='{"delay":0.1}' data-zanim-trigger="scroll">
+                      <div class="bg-holder rounded-left" style="background-image:url(/img/quinta/kitchen3.jpg);">
+                        <div class="bg-youtube" data-property='{"videoURL":"https://www.youtube.com/watch?v=Kra1eWAiKvE","startAt":0,"stopAt":57,"mute":true,"showYTLogo":false}'></div>
+                      </div>
+                      <!--/.bg-holder-->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- end of .container-->
+          </section>
+          <!-- <section> close ============================-->
+          <!-- ============================================-->
+    
+    
+          <!-- ============================================-->
+          <!-- <section> begin ============================-->
+          <section class="py-0" id="cafe-big-image">
+            <div class="container-fluid px-0">
+              <img class="img-fluid" src="/img/quinta/feel1.jpg" alt="" />
+            </div>
+            <!-- end of .container-->
+          </section>
+          <!-- <section> close ============================-->
+          <!-- ============================================-->
+    
+    
+    
+          <!-- ============================================-->
+          <!-- <section> begin ============================-->
+          <section id="cafe-contact">
+            <div class="container">
+              <div class="row justify-content-center mb-lg-6" data-zanim-timeline="{}" data-zanim-trigger="scroll">
+                <div class="col-md-10 col-lg-7 col-xl-6 text-center">
+                  <h2 class="text-underline mb-4 fs-3 fs-sm-4">Contact us!</h2>
+                  <p class="mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima iure quae perspiciatis illum esse non laboriosam?</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-6 order-1 order-lg-0 pr-lg-4">
+                  <form class="zform" method="post">
+                    <input class="form-control border-300" type="hidden" name="to" value="username@domain.extension" />
+                    <input class="form-control border-300" type="text" placeholder="Your Name" required="required" />
+                    <input class="form-control border-300 mt-3" type="text" placeholder="Phone Number" required="required" />
+                    <textarea class="form-control border-300 mt-3" rows="9" placeholder="Your Message" required="required"></textarea>
+                    <button class="btn btn-block btn-dark mt-4" type="submit"><span class="text-uppercase">Send</span><span class="fas fa-paper-plane text-white ml-1"></span></button>
+                    <div class="zform-feedback mt-4"></div>
+                  </form>
+                </div>
+                <div class="col-lg-6 pl-lg-4">
+                  <div class="googlemap rounded" data-latlng="37.057670,-8.885928" data-pov='{"heading":75,"pitch":0}' data-scrollwheel="false" data-icon="/img/map-marker.png" data-zoom="17" data-theme="Default" style="height: 21.88rem;">
+                    <div class="marker-content py-3">
+                      <h5>Quinta Al-Gharb</h5>
+                      <p>5 bungalows located
+                        <br/> in paradise.</p>
+                    </div>
+                  </div>
+                  <div class="row align-items-center mb-8 mb-lg-3 text-center text-lg-left mt-4">
+                    <div class="col-lg">
+                      <p class="mb-0"><span class="text-black font-weight-black">Address:</span><span> Estrada Praia da Ingrina,<br>Raposeira, 8601-904 Vila do Bispo. </span></p>
+                      <p class="mb-0"><span class="text-black font-weight-black">E-mail:</span><span class="font-italic"><a class="text-700" href="mailto:info@sparrow.com"> information@sparrow.com</a></span></p>
+                      <p class="mb-0"><span class="text-black font-weight-black">Phone:</span><span><a class="text-700" href="tel:+13234324534"> +13234324534</a></span></p>
+                    </div>
+                    <div class="col-lg-auto mt-3 mt-lg-0">
+                      <a class="btn btn-outline-dark btn-sm border-400 mr-2" href="#"><span class="fab fa-twitter"></span></a>
+                      <a class="btn btn-outline-dark btn-sm border-400 mr-2" href="#"><span class="fab fa-facebook-f"></span></a>
+                      <a class="btn btn-outline-dark btn-sm border-400" href="#"><span class="fab fa-google-plus-g"></span></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- end of .container-->
+          </section>
+          <!-- <section> close ============================-->
+          <!-- ============================================-->
+    
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>@yield('title')</title>
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/img/favicons/favicon.ico">
+    <link rel="manifest" href="/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="/lib/loaders.css/loaders.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Mono%7cPT+Serif:400,400i%7cLato:100,300,400,700,800,900" rel="stylesheet">
+    <link href="/lib/fancybox/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="/lib/jquery.mb.ytplayer/jquery.mb.YTPlayer.min.css" rel="stylesheet">
+    <link href="/css/theme.css" rel="stylesheet">
+  </head>
+
+  <body class="has-sidemenu has-sidemenu-right overflow-hidden-x">
+
+    <!--===============================================-->
+    <!--    Fancynav-->
+    <!--===============================================-->
+    <nav class="fancynavbar fancynavbar-right">
+      <div class="fancynavbar-togglerbar" data-onscroll-fade-in="true">
+        <a class="fancynavbar-brand" href="/">
+          <span class='fas fa-home fs-3'></span>
+          <!--You can use icon or text logo as well-->
+          <!--<span class='fab fa-superpowers fs-3'></span>-->
+          <!--<span class='logo-sparrow'>S</span>--></a>
+          <div class="fancynavbar-toggler">
+            <svg class="fancynavbar-toggler-icon" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg" data-zanim-lg='{"from":{"opacity":0,"x":45},"to":{"opacity":1,"x":0},"ease":"CubicBezier","duration":0.8,"delay":0.0}' data-zanim-trigger="scroll">
+              <path id="path-top" d="M20,25c0,0,22,0,30,0c16,0,18.89,40.71-.15,21.75C38.7,35.65,19.9,16.8,19.9,16.8"></path>
+              <path id="path-middle" d="M20,32h30"></path>
+              <path id="path-bottom" d="M19.9,46.98c0,0,18.8-18.85,29.95-29.95C68.89-1.92,66,38.78,50,38.78c-8,0-30,0-30,0"></path>
+            </svg>
+          </div>
+        <div class="fancynavbar-addon">
+          <a class="fancynavbar-addon-item" href="#"><span class="fab fa-facebook"></span></a>
+          <a class="fancynavbar-addon-item" href="#languageModal" data-toggle="modal"><span class="text-sans-serif ls font-weight-black fs--1 d-block">EN</span></a>
+        </div>
+      </div>
+      <div class="fancynavbar-collapse">
+        <ul class="fancynavbar-nav">
+          <li class="fancynav-item fancy-dropdown">
+            <a class="fancynav-link fancy-dropdown-toggle" href="JavaScript:void(0)"><span class="fancynav-link-content">bungalow</span></a>
+            <div class="fancy-dropdown-menu">
+              <div class="row pb-4 pt-3">
+                <div class="col-auto">
+                  <a class="fancy-dropdown-item" href="/">bungalow 1</a>
+                  <a class="fancy-dropdown-item" href="/">bungalow 2</a>
+                  <a class="fancy-dropdown-item" href="/">bungalow 3</a>
+                  <a class="fancy-dropdown-item" href="/">bungalow 4</a>
+                  <a class="fancy-dropdown-item" href="/">bungalow 5</a>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li class="fancynav-item">
+            <a class="fancynav-link" href="page-portfolio.html"><span class="fancynav-link-content">home</span></a>
+          </li>
+          <li class="fancynav-item">
+            <a class="fancynav-link" href="page-contact.html"><span class="fancynav-link-content">contact</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!--===============================================-->
+    <!--    End of Fancynav-->
+    <!--===============================================-->
+
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main min-vh-100" id="top">
+
+      <!-- ============================================-->
+      <!-- Preloader ==================================-->
+      <div class="preloader" id="preloader">
+        <div class="loader">
+          <div class="line-scale-pulse-out-rapid">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <!-- ============================================-->
+      <!-- End of Preloader ===========================-->
+
+@yield('body')
+
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+    <!--===============================================-->
+    <!--    Footer-->
+    <!--===============================================-->
+    <footer class="footer bg-black text-600 py-4 text-sans-serif text-center overflow-hidden" data-zanim-timeline="{}" data-zanim-trigger="scroll">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-4 order-lg-2">
+            <a class="indicator indicator-up" href="#top" data-fancyscroll="data-fancyscroll"><span class="indicator-arrow indicator-arrow-one" data-zanim-xs='{"from":{"opacity":0,"y":15},"to":{"opacity":1,"y":-5,"scale":1},"ease":"Back.easeOut","duration":0.4,"delay":0.9}'></span><span class="indicator-arrow indicator-arrow-two" data-zanim-xs='{"from":{"opacity":0,"y":15},"to":{"opacity":1,"y":-5,"scale":1},"ease":"Back.easeOut","duration":0.4,"delay":1.05}'></span></a>
+          </div>
+          <div class="col-lg-4 text-lg-left mt-4 mt-lg-0">
+            <p class="fs--1 text-uppercase ls font-weight-bold mb-0">Copyright &copy; 2020 FullStackDesign</p>
+          </div>
+          <div class="col-lg-4 text-lg-right order-lg-2 mt-2 mt-lg-0">
+            <p class="fs--1 text-uppercase ls font-weight-bold mb-0">@lang('layout.foot-love')<span class="text-danger fas fa-heart mx-1"></span>@lang('layout.foot-by')
+              <a class="text-600" href="https://FullStackDesign.ltd/">FullStackDesign</a>
+            </p>
+          </div>
+        </div>
+      </div>
+  </footer>
+    <!--===============================================-->
+    <!--    Modal for language selection-->
+    <!--===============================================-->
+    <!-- Modal-->
+    <div class="modal fade fade-in" id="languageModal" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-xs mx-auto" role="document">
+        <div class="modal-content bg-black">
+          <div class="modal-body text-center p-0">
+            <button class="close text-white position-absolute t-0 r-0 times-icon mt-2 mr-2 p-2" type="button" data-dismiss="modal" aria-label="Close"></button>
+            <ul class="list-unstyled pl-0 my-0 py-4 text-sans-serif">
+              <li>
+                <a class="text-white font-weight-bold pt-1 d-block" href="home-default.html">English</a>
+              </li>
+              <li>
+                <a class="pt-1 d-block text-500" href="#">Portugues</a>
+              </li>
+              <li>
+                <a class="pt-1 d-block text-500" href="#">Deutsche</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.js"></script>
+    <script src="/js/plugins.js"></script>
+    <script src="/lib/loaders.css/loaders.css.js"></script>
+    <script src="/js/stickyfill.min.js"></script>
+    <script src="/lib/is_js/is.min.js"></script>
+    <script src="/lib/jtap/jquery.tap.js"></script>
+    <script src="/js/rellax.min.js"></script>
+    <script src="/lib/fancybox/jquery.fancybox.min.js"></script>
+    <script src="/lib/jquery.mb.ytplayer/jquery.mb.YTPlayer.min.js"></script>
+    <script src="/js/theme.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARdVcREeBK44lIWnv5-iPijKqvlSAVwbw&callback=initMap" async></script>
+  </body>
+
+</html>
